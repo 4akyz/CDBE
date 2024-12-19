a61c55102228ddf257ffc0fccf2ed7d9e0b45167
4akyz <ajun4467@gmail.com>
2024-12-20 00:37:33 +0700

    pls2

---
+++ src/controller/product.js
@@ -0,0 +1,19 @@
+export const getProducts = (req,res) => {
+    console.log('req.query');
+}
+
+export const getProductById = (req,res) => {
+    console.log('getProductsById');
+}
+
+export const addProduct = (req,res) => {
+    console.log('addProduct');
+}
+
+export const deleteProduct = (req,res) => {
+    console.log('deleteProduct');
+}
+
+export const updateProduct = (req,res) => {
+    console.log('updateProduct');
+}
